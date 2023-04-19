9095b14d1f0e20191da2534ce1ed16c8cc7d826f
Claudio Villalobos <claudiodvillalobos@gmail.com>
2023-04-18 20:57:05 -0500

    Finished setup for this project

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
-from views import get_all_animals, get_single_animal, get_all_locations, get_single_location
+from views import (get_all_animals, get_single_animal, create_animal, delete_animal, update_animal, get_all_locations, get_single_location, create_location, delete_location, update_location, get_single_employee, get_all_employees, create_employee, delete_employee, update_employee, get_single_customer, get_all_customers, create_customer, delete_customer, update_customer)
 
 # Here's a class. It inherits from another class.
 # For now, think of a class as a container for functions that
@@ -59,6 +59,7 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any GET request.
     def do_GET(self):
+        """Gets objects"""
         self._set_headers(200)
         response = {}  # Default response
 
@@ -79,32 +80,163 @@ class HandleRequests(BaseHTTPRequestHandler):
             else:
                 response = get_all_locations()
 
+        if resource == "employees":
+            if id is not None:
+                response = get_single_employee(id)
+
+            else:
+                response = get_all_employees()
+
+        if resource == "customers":
+            if id is not None:
+                response = get_single_customer(id)
+
+            else:
+                response = get_all_customers()
+
         self.wfile.write(json.dumps(response).encode())
 
         # This weird code sends a response back to the client
         self.wfile.write(f"{response}".encode())
 
-    # Here's a method on the class that overrides the parent's method.
-    # It handles any POST request.
     def do_POST(self):
-        """Handles POST requests to the server
-        """
-        # Set response code to 'Created'
+        """Posts new objects"""
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+            self.wfile.write(json.dumps(new_animal).encode())
+
+
+        # Initialize new location
+        new_location = None
+
+        # Add a new location to the list. Don't worry about
+        # the orange squiggle, you'll define the create_location
+        # function next.
+        if resource == "locations":
+            new_location = create_location(post_body)
+
+
+        # Encode the new location and send in response
+            self.wfile.write(json.dumps(new_location).encode())
+
+         # Initialize new employee
+        new_employee = None
+
+        # Add a new employee to the list. Don't worry about
+        # the orange squiggle, you'll define the create_employee
+        # function next.
+        if resource == "employees":
+            new_employee = create_employee(post_body)
+
+        # Encode the new employee and send in response
+            self.wfile.write(json.dumps(new_employee).encode())
+
+        # Initialize new customer
+        new_customer = None
+
+        # Add a new customer to the list. Don't worry about
+        # the orange squiggle, you'll define the create_customer
+        # function next.
+        if resource == "customers":
+            new_customer = create_customer(post_body)
+
+
+        # Encode the new customer and send in response
+            self.wfile.write(json.dumps(new_customer).encode())
+
+    def do_DELETE(self):
+        """Delete Objects"""
+    # Set a 204 response code
+        self._set_headers(204)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            delete_animal(id)
+
+        # Encode the new animal and send in response
+            self.wfile.write("".encode())
+
+        # Delete a single location from the list
+        if resource == "locations":
+            delete_location(id)
+
+        # Encode the new location and send in response
+            self.wfile.write("".encode())
+
+        # Delete a single employee from the list
+        if resource == "employees":
+            delete_employee(id)
+
+        # Encode the new employee and send in response
+            self.wfile.write("".encode())
+
+        # Delete a single customer from the list
+        if resource == "customers":
+            delete_customer(id)
+
+        # Encode the new customer and send in response
+            self.wfile.write("".encode())
 
     # Here's a method on the class that overrides the parent's method.
     # It handles any PUT request.
 
     def do_PUT(self):
-        """Handles PUT requests to the server
-        """
-        self.do_POST()
+        self._set_headers(204)
+        content_len = int(self.headers.get('content-length', 0))
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
 
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Delete a single animal from the list
+        if resource == "animals":
+            update_animal(id, post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write("".encode())
+
+        # Delete a single location from the list
+        if resource == "locations":
+            update_location(id, post_body)
+
+        # Encode the new location and send in response
+        self.wfile.write("".encode())
+
+        # Delete a single employee from the list
+        if resource == "employees":
+            update_employee(id, post_body)
+
+        # Encode the new employee and send in response
+        self.wfile.write("".encode())
+
+        # Delete a single customer from the list
+        if resource == "customers":
+            update_customer(id, post_body)
+
+        # Encode the new customer and send in response
+        self.wfile.write("".encode())
 
 # This function is not inside the class. It is the starting
 # point of this application.
